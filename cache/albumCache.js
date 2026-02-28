326fadc376927ddba875ed1a211dae87fa33cdde
Luca <129414977+lucagithubs@users.noreply.github.com>
2026-02-28 15:58:45 +0100

    Add package.json configuration for Spotify bot

--- cache/albumCache.js
+++ cache/albumCache.js
@@ -1,1 +1,15 @@
-
+{
+  "name": "spotify-album-bot",
+  "version": "1.0.0",
+  "description": "Discord bot for Spotify album lookups",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js",
+    "deploy": "node deploy-commands.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.0",
+    "discord.js": "^14.14.1",
+    "dotenv": "^16.3.1"
+  }
+}
